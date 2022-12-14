077f6a5b454223f47d1df9a2ee4f7e85d310eb59
Ștefan Țîncu <stefantnc11@gmail.com>
2022-12-14 15:51:23 +0200

    Add API call for refreshing token
    Resolves: CU-3a35wep

--- src/services/general-services.ts
+++ src/services/general-services.ts
@@ -1,13 +1,17 @@
 import axios from 'axios';
 
-export async function refreshToken(token: string): Promise<any> {
-    try {
-        const response = await axios.get('http://localhost/api/auth/refresh', {
-            headers: { Authorization: `Bearer ${token}` },
-        });
+export function refreshToken(token: string, expiration: number): void {
+    window.setTimeout(async () => {
+        try {
+            const response = await axios.get('http://localhost/api/auth/refresh', {
+                headers: { Authorization: `Bearer ${token}` },
+            });
 
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
+            const receivedToken = response.data;
+            window.sessionStorage.setItem('token', JSON.stringify(receivedToken));
+            refreshToken(receivedToken.value, receivedToken.expiration);
+        } catch (error) {
+            throw error;
+        }
+    }, expiration - 1000 * 60);
 }
