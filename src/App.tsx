a696b14d5ad0eb6e09a46756a1cbceec7fd6f68f
Ștefan Țîncu <stefantnc11@gmail.com>
2022-12-14 14:24:56 +0200

    Initialize interceptor
    Resolves: CU-3a35wep

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import '@src/app.sass';
 import Router from '@src/router/router';
+import { initInterceptorHandler } from '@src/handlers/interceptor';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const App: React.FC<{ title: string; version: string }> = (props: { title: string; version: string }) => {
+    initInterceptorHandler();
     return <Router />;
 };
 
