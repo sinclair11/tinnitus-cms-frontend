a557ad9d0fe3ceb6e03f75a897412f11d3e4713d
Ștefan Țîncu <stefantnc11@gmail.com>
2022-12-14 14:22:51 +0200

    Add interceptor for requests and responses
    - Response with 401 redirects to login page
    - Each request after login is made with JWT in authorization header
    
    Resolves: CU-3a35wep

---
+++ src/handlers/interceptor.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+export function initInterceptorHandler(): void {
+    axios.defaults.headers.common['Content-Type'] = 'application/json';
+
+    axios.interceptors.response.use(
+        (response) => {
+            return response;
+        },
+        async (error: any) => {
+            if (error.response.status === 401) {
+                error.message = 'Unauthorized access';
+                return (window.location.href = '/login');
+            }
+            return Promise.reject(error);
+        },
+    );
+
+    axios.interceptors.request.use((config) => {
+        const tokenJSON = window.sessionStorage.getItem('token');
+        //Token available only after login
+        if (tokenJSON != null) {
+            const token = JSON.parse(tokenJSON);
+            config.headers.Authorization = `Bearer ${token.value}`;
+        }
+        return config;
+    });
+}
