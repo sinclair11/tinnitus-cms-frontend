04306f41a28b163edd4592272058f112fca2ad43
Ștefan Țîncu <stefantnc11@gmail.com>
2022-12-19 11:05:56 +0200

    Add module to store constants and config values

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+export const Endpoints = {
+    API_ROOT: 'http://localhost:8080',
+    API_LOGIN: 'http://localhost:8080/login',
+    API_GET_ALBUMS: 'http://localhost:8080/api/admin/albums',
+    API_ALBUM: 'http://localhost:8080/api/admin/album',
+    API_ALBUM_POST_AUDIO: 'http://localhost:8080/api/admin/album/audio',
+    API_ALBUM_POST_ARTWORK: 'http://localhost:8080/api/admin/album/artwork',
+    API_ALBUM_GET_AUDIO: 'http://localhost:8080/api/admin/album/assets/audio',
+    API_ALBUM_GET_ARTWORK: 'http://localhost:8080/api/admin/album/assets/artwork',
+};
